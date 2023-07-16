f46655230842395adfe1a413a47c3cc064f3e54c
Nathannicolle <nathan-nicolle@orange.fr>
2023-07-16 19:53:01 +0200

    retour sur de bonnes bases
    après correction grâce au cours
    
    en ce qui concerne la création d'une classe FaceSnap

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -7,14 +7,22 @@ import {FaceSnap} from "../models/face-snap.model";
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  creationDate!: Date;
+  snaps!: number;
+  imageURL!:string;
+  isSnapped!: boolean;
+  txtButton!: string;
+
   ngOnInit() {
-    let faceSnap1 = new FaceSnap();
-    faceSnap1.setTitle("A day as a dev !");
-    faceSnap1.setDescription("A cool work !");
-    faceSnap1.setImageURL("/assets/img/bg_site_pro_mini.jpg");
-    faceSnap1.setCreationDate(new Date(2023, 7, 14));
-    faceSnap1.setSnaps(0);
-    faceSnap1.setIsSnapped(false);
+    this.title = "A day as a dev!";
+    this.description = "That's fascinating !";
+    this.creationDate = new Date();
+    this.snaps = 3;
+    this.imageURL = "/assets/img/bg_site_pro_mini.jpg";
+    this.isSnapped = false;
+    this.txtButton = "Snap it 😃 !";
   }
 
   // Listeners
--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,15 +1,7 @@
 export class FaceSnap {
-  title: string;
-  description: string;
-  creationDate: Date;
-  snaps: number;
-  imageURL:string;
-
-  constructor(title:string, description:string, imageURL:string, creationDate:Date, snaps:number) {
-    this.title = title;
-    this.description = description;
-    this.imageURL = imageURL;
-    this.creationDate = creationDate;
-    this.snaps = snaps;
-  }
+  constructor(public title:string,
+              public description:string,
+              public imageURL:string,
+              public creationDate:Date,
+              public snaps:number) {}
 }
