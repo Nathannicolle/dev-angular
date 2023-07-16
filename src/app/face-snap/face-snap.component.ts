b62871ef430bea7243a95e7985301c82b7d4c630
Nathannicolle <nathan-nicolle@orange.fr>
2023-07-16 11:02:18 +0200

    essai d'instanciation de la classe FaceSnap

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FaceSnap} from "../models/face-snap.model";
 
 @Component({
   selector: 'app-face-snap',
@@ -6,21 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  creationDate!: Date;
-  snaps!: number;
-  imageURL!:string;
-  isSnapped!: boolean;
-  txtButton!: string;
-
   ngOnInit() {
-    this.title = "A day as a dev!";
-    this.description = "That's fascinating !";
-    this.creationDate = new Date();
-    this.snaps = 3;
-    this.imageURL = "/assets/img/bg_site_pro_mini.jpg";
-    this.isSnapped = false;
-    this.txtButton = "Snap it 😃 !";
+    let faceSnap1 = new FaceSnap();
+    faceSnap1.setTitle("A day as a dev !");
+    faceSnap1.setDescription("A cool work !");
+    faceSnap1.setImageURL("/assets/img/bg_site_pro_mini.jpg");
+    faceSnap1.setCreationDate(new Date(2023, 7, 14));
+    faceSnap1.setSnaps(0);
+    faceSnap1.setIsSnapped(false);
   }
 }
