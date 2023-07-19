ea723402fe902dc197f816f67e947836f777f192
Nathannicolle <nathan-nicolle@orange.fr>
2023-07-19 19:11:01 +0200

    parcours d'un tableau de faceSnaps
    qui contient des faceSnaps sous forme d'objets
    
    pour parcourir le tableau on utilise la directive *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,33 @@ import {FaceSnap} from "./models/face-snap.model";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  faceSnap!: FaceSnap;
-  faceSnap2!: FaceSnap;
-  faceSnap3!: FaceSnap
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.faceSnap = {
-      title: "A day as a dev!",
-      description: "That's fascinating !",
-      imageURL: "/assets/img/bg_site_pro_mini.jpg",
-      creationDate: new Date(),
-      snaps:3
-    }
-    this.faceSnap2 = {
-      title: "A wonderful app!",
-      description: "That's my first Angular App !",
-      imageURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/640px-Angular_full_color_logo.svg.png",
-      creationDate: new Date(),
-      snaps:3
-    }
-    this.faceSnap3 = {
-      title: "A wonderful app!",
-      description: "That's my first Angular App !",
-      imageURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/640px-Angular_full_color_logo.svg.png",
-      creationDate: new Date(),
-      snaps: 3,
-      location: "Miami"
-    }
+    this.faceSnaps = [
+      {
+        title: "A day as a dev!",
+        description: "That's fascinating !",
+        imageURL: "/assets/img/bg_site_pro_mini.jpg",
+        creationDate: new Date(),
+        snaps:3
+      },
+      {
+        title: "A wonderful app!",
+        description: "That's my first Angular App !",
+        imageURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/640px-Angular_full_color_logo.svg.png",
+        creationDate: new Date(),
+        snaps:3
+      },
+      {
+        title: "A wonderful app!",
+        description: "That's my first Angular App !",
+        imageURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/640px-Angular_full_color_logo.svg.png",
+        creationDate: new Date(),
+        snaps: 3,
+        location: "Miami"
+      }
+    ];
   }
 }
 
